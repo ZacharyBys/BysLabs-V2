fc3a41f71f427c896c3d174cb3bc6882150d3a54
ZacharyBys <zachary.bys@gmail.com>
2018-08-24 00:54:17 -0400

    Added timeline component with 4 first icons

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import '../styles/FrontPage.css';
 import IntroSection from './IntroSection';
+import Timeline from './Timeline';
 
 class FrontPage extends Component {
   render() {
     return (
       <div className="frontpage-background">
         <IntroSection/>
+        <Timeline/>
       </div>
     );
   }
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import '../styles/Timeline.css';
+import Concordia from '../resources/Concordia.png';
+import Eidos from '../resources/Eidos-Montreal.png';
+import Coveo from '../resources/Coveo.png';
+import HackConcordia from '../resources/HackConcordia.png';
 
 class Timeline extends Component {
   render() {
     return (
       <div className="timeline-container">
-
+        <table className="timeline-table">
+            <tr>
+                <td> </td>
+                <td><img src={Concordia}/></td>
+                <td><div className="timeline-connector"/></td>
+                <td><img src={Eidos}/></td>
+                <td><div className="timeline-connector"/></td>
+                <td><img src={Coveo}/></td>
+                <td><div className="timeline-connector"/></td>
+                <td><img src={HackConcordia}/></td>
+                <td> </td>
+            </tr>
+        </table>
       </div>
     );
   }
